402287b59e33d34f8e23ce9cd4140c6de136ce24
wzx <593735243@qq.com>
2018-12-25 18:08:07 +0800

    add recursive parse function

--- utils/transformparser.py
+++ utils/transformparser.py
@@ -1,5 +1,5 @@
 from torchvision import transforms
-from typing import List, Dict
+from typing import List, Dict, Any
 
 
 class TransformParser(object):
@@ -8,15 +8,33 @@ class TransformParser(object):
             net_params = dict()
         self.net_params = net_params
 
-    def _parse_single(self, single: dict):
+    def _parse_single_param(self, param: Any) -> Any:
+        # string, eval it
+        if isinstance(param, str):
+            return eval(param, self.net_params)
+        # list of params
+        elif isinstance(param, list):
+            return [self._parse_single_param(p) for p in param]
+            # ps = []
+            # for pa in param:
+            #     ps.append(self._parse_single_param(pa))
+            # return ps
+        # dict, parse as a transform
+        elif isinstance(param, dict):
+            return self._parse_single_transform(param)
+        else:
+            return param
+
+    def _parse_single_transform(self, single: dict):
         tmodule = transforms if 'module' not in single else __import__(
             single['module'], fromlist=[single['name']])
         topt = getattr(tmodule, single['name'])
 
         params: dict = single['params']
         for k, v in params.items():
-            if isinstance(v, str) and v in self.net_params:
-                params[k] = self.net_params[v]
+            # if isinstance(v, str) and v in self.net_params:
+            #     params[k] = self.net_params[v]
+            params[k] = self._parse_single_param(v)
 
         if 'multi' in single:
             multi = single['multi']
@@ -28,11 +46,11 @@ class TransformParser(object):
     def parse(self, translist: List[dict]):
         ts = []
         for trans in translist:
-            t, multi = self._parse_single(trans)
+            t, multi = self._parse_single_transform(trans)
 
             if multi:
                 ts.extend(t)
             else:
                 ts.append(t)
-        
+
         return transforms.Compose(ts)
