b02e82611b9a6b5f9f2ba022dcae0b36646a7f45
wzx <593735243@qq.com>
2018-12-28 10:48:36 +0800

    converter for json to yaml

---
+++ utils/json2yaml.py
@@ -0,0 +1,13 @@
+import json
+import yaml
+import sys
+import os
+
+if __name__ == '__main__':
+    json_file = sys.argv[1]
+    if len(sys.argv) >= 2:
+        yaml_file = sys.argv[2]
+    else:
+        yaml_file = f"{os.path.splitext(json_file)[0]}.yaml"
+
+    yaml.dump(json.load(open(json_file, 'r')), open(yaml_file, 'w'))
